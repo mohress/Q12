ac5d419fbb9b506ccc48bff568d1b21c842aebb7
Mohammed Saddam Sedeeq <mohamedabasy073@gmail.com>
2026-07-08 12:10:45 +0300

    build: configure Android platform and BLE plugins
    Initialize Android project structure, add Bluetooth connectivity 
    plugins, and update dependencies for Capacitor support.

---
+++ android/app/src/main/assets/public/cordova_plugins.js
@@ -0,0 +1,29 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "cordova-plugin-ble-central.ble",
+          "file": "plugins/cordova-plugin-ble-central/www/ble.js",
+          "pluginId": "cordova-plugin-ble-central",
+        "clobbers": [
+          "ble"
+        ]
+        },
+      {
+          "id": "cordova-plugin-bluetooth-serial.bluetoothSerial",
+          "file": "plugins/cordova-plugin-bluetooth-serial/www/bluetoothSerial.js",
+          "pluginId": "cordova-plugin-bluetooth-serial",
+        "clobbers": [
+          "window.bluetoothSerial"
+        ]
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "cordova-plugin-ble-central": "2.0.0",
+      "cordova-plugin-bluetooth-serial": "0.4.7"
+    };
+    // BOTTOM OF METADATA
+    });
+    
---
+++ android/app/src/main/java/com/alwa/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.alwa.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
