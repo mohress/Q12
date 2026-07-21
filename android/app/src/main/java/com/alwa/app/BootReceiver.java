c4d2cdee79ace71b7c99643048b76b3607a983fd
Mohammed Saddam Sedeeq <mohamedabasy073@gmail.com>
2026-07-21 23:51:59 +0300

    ة

--- android/app/src/main/java/com/alwa/app/BootReceiver.java
+++ android/app/src/main/java/com/alwa/app/BootReceiver.java
@@ -8,24 +8,19 @@ import android.os.Looper;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(final Context context, Intent intent) {
+    public void onReceive(final Context context, final Intent intent) {
         String action = intent.getAction();
         if (Intent.ACTION_BOOT_COMPLETED.equals(action) ||
             "android.intent.action.LOCKED_BOOT_COMPLETED".equals(action) ||
-            "android.intent.action.QUICKBOOT_POWERON".equals(action) ||
-            "com.htc.intent.action.QUICKBOOT_POWERON".equals(action)) {
+            "android.intent.action.QUICKBOOT_POWERON".equals(action)) {
             
-            // Add a 2500ms delay before launching MainActivity to ensure WebView and system services start
+            // Add a 2500ms delay before launching MainActivity
             new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    try {
-                        Intent launchIntent = new Intent(context, MainActivity.class);
-                        launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                        context.startActivity(launchIntent);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    Intent launchIntent = new Intent(context, MainActivity.class);
+                    launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                    context.startActivity(launchIntent);
                 }
             }, 2500);
         }
